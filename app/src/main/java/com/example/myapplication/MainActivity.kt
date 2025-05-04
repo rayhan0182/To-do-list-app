ee11609490ef2601961f7f51853a744ffc58e6e8
IC <md.rayhan8045@gmail.com>
2025-05-04 06:35:30 -0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+
+            startActivity(Intent(this,MainActivity2::class.java))
+
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.homepage.setOnClickListener {
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+        }
+
+
+
+
+
+    }
+}
